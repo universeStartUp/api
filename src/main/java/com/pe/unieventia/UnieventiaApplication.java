d2f35eb6d62334903ac86170e99c4eb048bf06c5
MarcDiazQ <89035955+MarcDiazQ@users.noreply.github.com>
2023-09-25 19:22:29 -0500

    base_unieventia
    first commit

---
+++ src/main/java/com/pe/unieventia/UnieventiaApplication.java
@@ -0,0 +1,13 @@
+package com.pe.unieventia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnieventiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnieventiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/unieventia/UnieventiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.unieventia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnieventiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

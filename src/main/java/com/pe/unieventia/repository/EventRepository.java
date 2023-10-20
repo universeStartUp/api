fb27f53ca54ee1e969e0c2c3de231c3e2ae7b700
JosephRamirez-F4fu <bensamuel114@gmail.com>
2023-10-20 05:02:37 -0500

    build eventState service

--- src/main/java/com/pe/unieventia/repository/EventRepository.java
+++ src/main/java/com/pe/unieventia/repository/EventRepository.java
@@ -1,10 +1,14 @@
 package com.pe.unieventia.repository;
 
 import com.pe.unieventia.entity.Event;
+import com.pe.unieventia.entity.EventState;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event,Long> {
+    boolean existsByEventState_Id(Long eventState_id);
 
 }
--- src/main/java/com/pe/unieventia/repository/EventStateRepository.java
+++ src/main/java/com/pe/unieventia/repository/EventStateRepository.java
@@ -1,4 +1,10 @@
 package com.pe.unieventia.repository;
 
-public interface EventStateRepository {
+import com.pe.unieventia.entity.EventState;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventStateRepository extends JpaRepository<EventState,Long> {
+    Boolean existsByName(String name);
 }

dc2c50c8e17f6a4204592785cd387cbee3c95cf6
Joseph Ramirez Sarmiento <U20211C828@upc.edu.pe>
2023-10-27 20:38:03 -0500

    restructure project

--- src/main/java/com/pe/unieventia/shared/mapper/modelMapperConfig.java
+++ src/main/java/com/pe/unieventia/shared/mapper/modelMapperConfig.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class modelMapperConfig {
+public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
-    }  
+    }
 }

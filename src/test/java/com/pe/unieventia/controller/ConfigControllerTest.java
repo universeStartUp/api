df340991e3e0c3bd5be13eb3954cd80d21ea72aa
MarcDiazQ <u202021530@upc.edu.pe>
2023-11-19 08:04:25 -0500

    new test for config added

--- src/test/java/com/pe/unieventia/controller/ConfigControllerTest.java
+++ src/test/java/com/pe/unieventia/controller/ConfigControllerTest.java
@@ -1,5 +1,6 @@
-package com.pe.unieventia.config.api;
+package com.pe.unieventia.controller;
 
+import com.pe.unieventia.config.api.ConfigController;
 import com.pe.unieventia.config.domain.entity.Config;
 import com.pe.unieventia.config.domain.service.ConfigService;
 import com.pe.unieventia.config.dto.ConfigDto;
@@ -14,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
@@ -33,20 +35,42 @@ public class ConfigControllerTest {
     public void testGetConfig() {
         Long id = 1L;
         Config config = new Config();
-        config.setEmailEnabled(true); // ajusta según tu entidad Config
-        config.setSmsEnabled(false); // ajusta según tu entidad Config
+        config.setEmailEnabled(true);
+        config.setSmsEnabled(false);
         config.setReceiveNewsletter(false);
         config.setLanguagePreference("es");
-        // Configura el resto de las propiedades de Config según sea necesario
 
         when(configService.getNotificationConfig(anyLong())).thenReturn(Optional.of(config));
 
         ResponseEntity<ConfigDto> response = configController.getConfig(id);
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        // Aquí deberías verificar también los campos específicos del DTO, como se hace en tus pruebas de evento
+        // verificar también los campos específicos del DTO
     }
 
-    // Agrega más pruebas para otros métodos como POST, etc.
-}
+    @Test
+    public void testUpdateConfig() {
+        ConfigDto configDto = new ConfigDto();
+        // Configura ConfigDto
+        configDto.setEmailEnabled(true);
+        configDto.setSmsEnabled(false);
+        configDto.setReceivedNewsletter(false);
+        configDto.setLanguagePreference("es");
 
+        Config config = new Config();
+        // Simula la conversión del DTO a la entidad Config
+        config.setEmailEnabled(configDto.isEmailEnabled());
+        config.setSmsEnabled(configDto.isSmsEnabled());
+        config.setReceiveNewsletter(configDto.isReceivedNewsletter());
+        config.setLanguagePreference(configDto.getLanguagePreference());
+
+        // Simula la respuesta esperada del servicio
+        when(configService.saveNotificationConfig(any(Config.class))).thenReturn(config);
+
+        ResponseEntity<Config> response = configController.updateConfig(configDto);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(config, response.getBody());
+    }
+
+}
